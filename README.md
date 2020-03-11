6201d7f9f6e3d56a4f19c7529a4da636130ea072
Jim Lengel <lengel@Jims-Mac-mini.local>
2020-03-11 12:42:07 -0700

    added explanations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tipcalculator
+
+This project takes an input and computes a tip (20%). It then puts that amount on the page.
--- tipcalc.js
+++
@@ -1,4 +0,0 @@
-function calcTip(bill){
-	var totalBill = bill * 1.2
-	document.getElementById('total').innerHTML = totalBill
-}
